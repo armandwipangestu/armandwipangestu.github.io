b0aaf0b217491eb3fac9fd4e5f60ad90750a1046
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-08-30 21:06:55 +0000

    stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MainLayouts from "./components/Layouts/main.layouts";
 import Navigation from "./components/Layouts/navigation.layouts";
+import About from "./components/Contents/about.contents";
 
 const App = () => {
   return (
     <MainLayouts>
       <Navigation />
+      <About />
     </MainLayouts>
   );
 };
