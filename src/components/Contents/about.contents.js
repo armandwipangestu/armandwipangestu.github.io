c771e15d2e4fd69968110a62c6f42dd92325c433
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-08-30 21:06:06 +0000

    add component contents

---
+++ src/components/Contents/about.contents.js
@@ -0,0 +1,11 @@
+import Me from "../../Assets/Images/Me/me.png";
+
+const About = () => {
+  return (
+    <div className="flex items-center justify-center mt-20">
+      <img src={Me} className="object-scale-down object-center h-56 w-120" />
+    </div>
+  );
+};
+
+export default About;
