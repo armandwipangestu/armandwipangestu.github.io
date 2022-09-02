12e7140beb33022b470d7d79b894a39078d6738f
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-09-02 22:45:19 +0000

    Components: Edit About

--- src/components/Contents/about.contents.js
+++ src/components/Contents/about.contents.js
@@ -1,12 +1,24 @@
 import Me from "../../Assets/Images/Me/me.png";
-import Card from "../Utilities/card.github";
 
 const About = () => {
   return (
     <>
-      <div className="flex items-center justify-center mt-20">
-        {/* <img src={Me} className="object-scale-down object-center h-56 w-120" /> */}
-        <Card />
+      <div className="p-6">
+        <div className="flex text-3xl justify-center">
+          <h3 className="font-bold" id="about">
+            About
+          </h3>
+        </div>
+      </div>
+      <div className="grid justify-items-center">
+        <img src={Me} className="object-scale-down object-center h-56 w-120" />
+        <p className="text-gray-600 mt-5 md:px-56 px-10">
+          Laborum in excepteur laborum dolor fugiat pariatur excepteur
+          consectetur. Est laborum sint irure non non esse nisi non proident
+          sit. Et labore reprehenderit nostrud aliquip incididunt incididunt ea
+          ut nostrud. Id esse do pariatur elit. Anim officia anim consequat est
+          aliquip aute in do deserunt anim.
+        </p>
       </div>
     </>
   );
