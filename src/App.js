6d63b38e87ac083e086b41018450e48fbb3a4079
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-09-02 22:46:06 +0000

    App: add new component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import MainLayouts from "./components/Layouts/main.layouts";
 import Navigation from "./components/Layouts/navigation.layouts";
 import About from "./components/Contents/about.contents";
+import Projects from "./components/Contents/project.contents";
+import Skills from "./components/Contents/skill.contents";
 
 const App = () => {
   return (
     <MainLayouts>
       <Navigation />
       <About />
+      <Projects />
+      <Skills />
     </MainLayouts>
   );
 };
