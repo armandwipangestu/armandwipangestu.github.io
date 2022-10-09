445960a24d3dba194aea92dddfec7327145897d6
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-10-09 20:31:37 +0700

    core: add new component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import Navigation from "./components/Layouts/navigation.layouts";
 import About from "./components/Contents/about.contents";
 import Projects from "./components/Contents/project.contents";
 import Skills from "./components/Contents/skill2.contents";
+import Contact from "./components/Contents/contact.contents";
+import Profile from "./components/Contents/profile.contents";
+import Experience from "./components/Contents/experience.contents";
 
 const App = () => {
   return (
     <MainLayouts>
       <Navigation />
       <About />
+      <Profile />
+      <Experience />
       <Projects />
       <Skills />
+      <Contact />
     </MainLayouts>
   );
 };
