d26728a5ad5492557e2834f3da1cff1e7fdd38d7
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-10-09 20:33:34 +0700

    core: change structure import

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import MainLayouts from "./components/Layouts/main.layouts";
 import Navigation from "./components/Layouts/navigation.layouts";
 import About from "./components/Contents/about.contents";
+import Profile from "./components/Contents/profile.contents";
+import Experience from "./components/Contents/experience.contents";
 import Projects from "./components/Contents/project.contents";
 import Skills from "./components/Contents/skill2.contents";
 import Contact from "./components/Contents/contact.contents";
-import Profile from "./components/Contents/profile.contents";
-import Experience from "./components/Contents/experience.contents";
 
 const App = () => {
   return (
