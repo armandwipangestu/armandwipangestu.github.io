61b0d19532ec9bf19e4b7ae5548c43f560d71aa9
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-10-09 20:32:29 +0700

    component: add new component profile, experience, and contact

---
+++ src/components/Contents/experience.contents.js
@@ -0,0 +1,26 @@
+const Experience = () => {
+  return (
+    <section id="experience">
+      <div className="grid justify-items-center">
+        <div className="pt-6">
+          <div className="flex text-3xl justify-center">
+            <h3 className="font-bold">Experience</h3>
+          </div>
+        </div>
+        <h4 className="font-bold pt-4">
+          PT. CITRA JELAJAH INFORMATIKA (2021 - Now)
+        </h4>
+        <p className="text-gray-600 mt-5 md:px-56 px-10 text-xs md:text-base text-bold">
+          • Menganalisa, merancang, instalasi kebutuhan jaringan di gedung, mall
+          <br />
+          • Network Operation Center (NOC)
+          <br />
+          • Network Engineer
+          <br />• Monitoring Server
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
---
+++ src/components/Contents/profile.contents.js
@@ -0,0 +1,21 @@
+const Profile = () => {
+  return (
+    <section id="profile">
+      <div className="grid justify-items-center">
+        <div className="pt-6">
+          <div className="flex text-3xl justify-center">
+            <h3 className="font-bold">Profile</h3>
+          </div>
+        </div>
+        <p className="text-gray-600 mt-5 md:px-56 px-10 text-xs md:text-base">
+          Saya seorang Web Developer dan Network Engineer yang mampu membangun
+          aplikasi berbasis website menggunakan stack teknologi modern serta
+          mampu merancang, menganalisa sebuah topologi jaringan dan
+          mengoperasikan perangkat jaringan.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
